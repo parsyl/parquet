a926c66338bd07d9c327abbd5f49707f613e9942
Craig Swank <craigswank@Craigs-MacBook-Pro.local>
2019-01-23 17:28:53 -0700

    added a python writer

---
+++ example/write.py
@@ -0,0 +1,11 @@
+import pyarrow.parquet as pq
+import pyarrow as pa
+import pandas as pd
+
+def main():
+    df = pd.read_json("./records.json")
+    table = pa.Table.from_pandas(df)
+    pq.write_table(table, 'py_records.parquet')
+
+if __name__ == "__main__":
+    main()
