4fba131ea69b700f07596491070d0968c8338662
Craig Swank <craigswank@gmail.com>
2019-01-25 10:48:27 -0700

    wip

--- examples/people/main.go
+++ examples/people/main.go
@@ -3,8 +3,6 @@ package main
 //go:generate parquetgen -type Person -package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"math"
 	"os"
@@ -19,20 +17,12 @@ func main() {
 	defer f.Close()
 
 	w := NewParquetWriter(f)
-	jf, err := os.Open("people.json")
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	var people []Person
-	if err := json.NewDecoder(jf).Decode(&people); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("people", len(people))
-
-	for i, person := range people {
-		person.Birthday = math.MaxUint32 - uint32(i+1)
-		w.Add(person)
+	for i := 0; i < 2000; i++ {
+		w.Add(Person{
+			ID:       i,
+			Birthday: math.MaxUint32 - uint32(i+1),
+		})
 	}
 
 	if err := w.Write(); err != nil {
