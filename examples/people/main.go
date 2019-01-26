e87a1fc5955e0730322c8c918c6dad8f7ab3b616
Craig Swank <craigswank@gmail.com>
2019-01-26 10:21:56 -0700

    move stuff around in examples/people
    This helps illustrate in main.go the bare minimum code that
    the user needs to write.

--- examples/people/main.go
+++ examples/people/main.go
@@ -4,17 +4,8 @@ package main
 
 import (
 	"log"
-	"math"
-	"math/rand"
 	"os"
-	"time"
 )
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
-
-var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func main() {
 	f, err := os.Create("people.parquet")
@@ -35,53 +26,8 @@ func main() {
 	}
 }
 
-func newPerson(i int) Person {
-	var age *int32
-	if i%2 == 0 {
-		a := int32(20 + i%5)
-		age = &a
-	}
-
-	var sadness *int64
-	if i%3 == 0 {
-		s := int64(i + 5)
-		sadness = &s
-	}
-
-	var lameness *float32
-	if i%4 == 0 {
-		l := rand.Float32()
-		lameness = &l
-	}
-
-	var keen *bool
-	if i%5 == 0 {
-		b := true
-		keen = &b
-	}
-
-	var anv *uint64
-	if i%3 == 0 {
-		x := math.MaxUint64 - uint64(i*100)
-		anv = &x
-	}
-
-	return Person{
-		Being: Being{
-			ID:  int32(i),
-			Age: age,
-		},
-		Happiness:   int64(i * 2),
-		Sadness:     sadness,
-		Code:        randString(8),
-		Funkiness:   rand.Float32(),
-		Lameness:    lameness,
-		Keen:        keen,
-		Birthday:    uint32(i * 1000),
-		Anniversary: anv,
-	}
-}
-
+// Being is split out only to show how embedded structs
+// are handled.
 type Being struct {
 	ID  int32  `parquet:"id"`
 	Age *int32 `parquet:"age"`
@@ -97,12 +43,7 @@ type Person struct {
 	Keen        *bool    `parquet:"keen"`
 	Birthday    uint32   `parquet:"birthday"`
 	Anniversary *uint64  `parquet:"anniversary"`
-}
 
-func randString(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letterRunes[rand.Intn(len(letterRunes))]
-	}
-	return string(b)
+	// Secret will not be part of parquet.
+	Secret string `parquet:"-"`
 }
