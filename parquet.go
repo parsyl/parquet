9cb18ff130bf0b768f31b5150103d4398cad7aca
Craig Swank <craigswank@gmail.com>
2019-01-26 13:25:02 -0700

    change root name

--- parquet.go
+++ parquet.go
@@ -97,7 +97,7 @@ func (m *Metadata) Footer(w io.Writer) error {
 	rgs := make([]*sch.RowGroup, len(m.rowGroups))
 	for i, rg := range m.rowGroups {
 		for _, col := range rg.fields {
-			if col.Name == "parquet_go_root" {
+			if col.Name == "root" {
 				continue
 			}
 
@@ -174,7 +174,7 @@ func schemaElements(fields []Field) []*sch.SchemaElement {
 	rt := sch.FieldRepetitionType_REQUIRED
 	out[0] = &sch.SchemaElement{
 		RepetitionType: &rt,
-		Name:           "parquet_go_root",
+		Name:           "root",
 		NumChildren:    &l,
 	}
 
