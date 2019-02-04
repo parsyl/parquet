c6ef1d0da38f2f9cb5bfb683bf9fe845bc4b5d16
Craig Swank <craigswank@gmail.com>
2019-02-04 07:06:40 -0700

    make -ignore default true

--- cmd/parquetgen/main.go
+++ cmd/parquetgen/main.go
@@ -15,7 +15,7 @@ var (
 	pkg    = flag.String("package", "", "package name")
 	imp    = flag.String("import", "", "the type's import statement (only if it doesn't live in 'package')")
 	pth    = flag.String("input", "", "path to the go file that defines -type")
-	ignore = flag.Bool("ignore", false, "ignore unsupported fields in -type")
+	ignore = flag.Bool("ignore", true, "ignore unsupported fields in -type")
 )
 
 func main() {
