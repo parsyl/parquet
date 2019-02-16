f54b4a11f965b5cfd0855962e876a29263812190
Craig Swank <craigswank@gmail.com>
2019-02-16 06:29:46 -0700

    more rle refactoring

--- rle.go
+++ rle.go
@@ -7,6 +7,11 @@ import (
 	"math/bits"
 )
 
+const (
+	mask1 = uint64(0x7F)
+	mask2 = uint64(0x80)
+)
+
 // WriteLevels writes vals to w as RLE encoded data
 func WriteLevels(w io.Writer, levels []int64) error {
 	var max uint32
@@ -14,91 +19,88 @@ func WriteLevels(w io.Writer, levels []int64) error {
 		max = 1
 	}
 
-	rleBuf := writeRLE(levels, int32(bits.Len32(max)))
-	res := make([]byte, 0)
-	binary.Write(w, binary.LittleEndian, int32(len(rleBuf)))
-	res = append(res, rleBuf...)
-	_, err := w.Write(res)
+	rle := writeRLE(levels, int32(bits.Len32(max)))
+	var out []byte
+	binary.Write(w, binary.LittleEndian, int32(len(rle)))
+	out = append(out, rle...)
+	_, err := w.Write(out)
 	return err
 }
 
-func writeRLE(levels []int64, bitWidth int32) []byte {
-	ln := len(levels)
-	var i int
-	var out []byte
-	for i < ln {
-		j := i + 1
-		for j < ln && levels[j] == levels[i] {
-			j++
+func writeRLE(levels []int64, width int32) []byte {
+	l := len(levels)
+	var i, reps int
+	var out, val []byte
+	for i < l {
+		reps = i + 1
+		for reps < l && levels[reps] == levels[i] {
+			reps++
 		}
 		var buf bytes.Buffer
 		binary.Write(&buf, binary.LittleEndian, levels[i])
-		val := buf.Bytes()
-		bn := (bitWidth + 7) / 8
-		out = append(out, append(header(uint64(j-i)), val[0:bn]...)...)
-		i = j
+		val = buf.Bytes()
+		out = append(out, append(header(uint64(reps-i)), val[0:(width+7)/8]...)...)
+		i = reps
 	}
 	return out
 }
 
 func header(n uint64) []byte {
-	n = n << 1
-	byteNum := (bits.Len64(n) + 6) / 7
-	if byteNum == 0 {
+	n <<= 1
+	l := (bits.Len64(n) + 6) / 7
+	if l == 0 {
 		return make([]byte, 1)
 	}
-	res := make([]byte, byteNum)
+	out := make([]byte, l)
 	tmp := n
-	for i := 0; i < int(byteNum); i++ {
-		res[i] = byte(tmp & uint64(0x7F))
-		res[i] = res[i] | byte(0x80)
+	for i := 0; i < int(l); i++ {
+		out[i] = (byte(tmp & mask1)) | byte(mask2)
 		tmp = tmp >> 7
 	}
-	res[byteNum-1] &= byte(0x7F)
-	return res
+	out[l-1] &= byte(mask1)
+	return out
 }
 
 // ReadLevels reads the RLE encoded definition levels
-func ReadLevels(r io.Reader) ([]int64, int, error) {
-	bitWidth := uint64(bits.Len64(1)) //TODO: figure out if this is correct
-	res := make([]int64, 0)
+func ReadLevels(in io.Reader) ([]int64, int, error) {
+	width := uint64(bits.Len64(1)) //TODO: figure out if this is correct
+	var out []int64
 	var l int32
-	if err := binary.Read(r, binary.LittleEndian, &l); err != nil {
-		return res, 0, err
+	if err := binary.Read(in, binary.LittleEndian, &l); err != nil {
+		return out, 0, err
 	}
 
 	buf := make([]byte, l)
-	if _, err := r.Read(buf); err != nil {
-		return res, 0, err
+	if _, err := in.Read(buf); err != nil {
+		return out, 0, err
 	}
 
-	newReader := bytes.NewReader(buf)
-	for newReader.Len() > 0 {
-		header, err := readUint64(newReader)
+	r := bytes.NewReader(buf)
+	for r.Len() > 0 {
+		header, err := readUint64(r)
 		if err != nil {
-			return res, 0, err
+			return out, 0, err
 		}
 		if header&1 == 0 {
-			buf, err := readRLE(newReader, header, bitWidth)
+			buf, err := readRLE(r, header, width)
 			if err != nil {
-				return res, 0, err
+				return out, 0, err
 			}
-			res = append(res, buf...)
+			out = append(out, buf...)
 
 		} else {
-			buf, err := readRLEBitPacked(newReader, header, bitWidth)
+			buf, err := readRLEBitPacked(r, header, width)
 			if err != nil {
-				return res, 0, err
+				return out, 0, err
 			}
-			res = append(res, buf...)
+			out = append(out, buf...)
 		}
 	}
-	return res, int(l + 4), nil
+	return out, int(l + 4), nil
 }
 
 func readRLEBitPacked(r io.Reader, header, width uint64) ([]int64, error) {
-	nGroups := header >> 1
-	count := nGroups * 8
+	count := (header >> 1) * 8
 	if width == 0 {
 		return make([]int64, count), nil
 	}
@@ -109,8 +111,8 @@ func readRLEBitPacked(r io.Reader, header, width uint64) ([]int64, error) {
 		return nil, err
 	}
 
-	currentByte := 0
-	data := uint64(rawBytes[currentByte])
+	current := 0
+	data := uint64(rawBytes[current])
 	mask := uint64((1 << width) - 1)
 	left := uint64(8)
 	right := uint64(0)
@@ -125,9 +127,9 @@ func readRLEBitPacked(r io.Reader, header, width uint64) ([]int64, error) {
 			out = append(out, int64((data>>right)&mask))
 			total -= width
 			right += width
-		} else if currentByte+1 < len(rawBytes) {
-			currentByte++
-			data |= uint64(rawBytes[currentByte] << left)
+		} else if current+1 < len(rawBytes) {
+			current++
+			data |= uint64(rawBytes[current] << left)
 			left += 8
 		}
 	}
@@ -151,11 +153,6 @@ func readRLE(r io.Reader, header uint64, bitWidth uint64) ([]int64, error) {
 	}
 	return out, nil
 }
-
-const (
-	mask1 = uint64(0x7F)
-	mask2 = uint64(0x80)
-)
 
 func readUint64(r io.Reader) (uint64, error) {
 	var err error
