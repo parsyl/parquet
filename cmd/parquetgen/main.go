32767c5c1ffcf056ce3c9bbb4c28baaee434a688
Craig Swank <craigswank@gmail.com>
2019-03-02 17:05:36 -0700

    git it working

--- cmd/parquetgen/main.go
+++ cmd/parquetgen/main.go
@@ -16,34 +16,55 @@ import (
 )
 
 var (
-	typ    = flag.String("type", "", "name of the struct that will used for writing and reading")
-	pkg    = flag.String("package", "", "package of the generated code")
-	imp    = flag.String("import", "", "import statement of -type if it doesn't live in -package")
-	pth    = flag.String("input", "", "path to the go file that defines -type")
-	outPth = flag.String("output", "parquet.go", "name of the file that is produced, defaults to parquet.go")
-	ignore = flag.Bool("ignore", true, "ignore unsupported fields in -type, otherwise log.Fatal is called when an unsupported type is encountered")
-	parq   = flag.String("parquet", "", "path to a parquet file")
+	typ          = flag.String("type", "", "name of the struct that will used for writing and reading")
+	pkg          = flag.String("package", "", "package of the generated code")
+	imp          = flag.String("import", "", "import statement of -type if it doesn't live in -package")
+	pth          = flag.String("input", "", "path to the go file that defines -type")
+	outPth       = flag.String("output", "parquet.go", "name of the file that is produced, defaults to parquet.go")
+	ignore       = flag.Bool("ignore", true, "ignore unsupported fields in -type, otherwise log.Fatal is called when an unsupported type is encountered")
+	parq         = flag.String("parquet", "", "path to a parquet file")
+	structOutPth = flag.String("struct-output", "generated_struct.go", "name of the file that is produced, defaults to parquet.go")
+
+	parquetTypes = map[string]string{
+		"BOOLEAN":    "bool",
+		"INT32":      "int32",
+		"INT64":      "int64",
+		"FLOAT":      "float32",
+		"DOUBLE":     "float64",
+		"BYTE_ARRAY": "string",
+	}
+
+	funcs = template.FuncMap{
+		"removeStar": func(s string) string {
+			return strings.Replace(s, "*", "", 1)
+		},
+		"titlecase": func(s string) string {
+			return strings.Title(s)
+		},
+		"dedupe": func(fields []parse.Field) []parse.Field {
+			seen := map[string]bool{}
+			out := make([]parse.Field, 0, len(fields))
+			for _, f := range fields {
+				_, ok := seen[f.FieldType]
+				if !ok {
+					out = append(out, f)
+					seen[f.FieldType] = true
+				}
+			}
+			return out
+		},
+	}
 )
 
-var funcs = template.FuncMap{
-	"removeStar": func(s string) string {
-		return strings.Replace(s, "*", "", 1)
-	},
-	"titlecase": func(s string) string {
-		return strings.Title(s)
-	},
-	"dedupe": func(fields []parse.Field) []parse.Field {
-		seen := map[string]bool{}
-		out := make([]parse.Field, 0, len(fields))
-		for _, f := range fields {
-			_, ok := seen[f.FieldType]
-			if !ok {
-				out = append(out, f)
-				seen[f.FieldType] = true
-			}
-		}
-		return out
-	},
+type structField struct {
+	Name string
+	Type string
+}
+
+type newStruct struct {
+	Package    string
+	StructName string
+	Fields     []structField
 }
 
 type fieldType struct {
@@ -54,32 +75,24 @@ type fieldType struct {
 func main() {
 	flag.Parse()
 	if *parq == "" {
-		fromStruct()
+		fromStruct(*pth)
 	} else {
 		fromParquet()
 	}
 }
 
-type structField struct {
-	Name string
-	Type string
-}
-
-type newStruct struct {
-	Name   string
-	Fields []structField
-}
-
 func fromParquet() {
-	f, err := os.Open(*parq)
+	pf, err := os.Open(*parq)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	footer, err := parquet.ReadFooter(f)
+	footer, err := parquet.ReadFooter(pf)
 	if err != nil {
 		log.Fatal("couldn't read footer: ", err)
 	}
+
+	pf.Close()
 
 	tmpl := template.New("output").Funcs(funcs)
 	tmpl, err = tmpl.Parse(structTpl)
@@ -99,36 +112,39 @@ func fromParquet() {
 			Type: getFieldType(s),
 		}
 	}
-}
-
-func getFieldType(se *sch.SchemaElement) string {
-	if se.Type == nil {
-		log.Fatal("nil parquet schema type")
-	}
-	s := se.Type.String()
-	out, ok := parquetTypes[s]
-	if !ok {
-		log.Fatal("unsupported parquet schema type: %s", s)
+	n := newStruct{
+		Package:    *pkg,
+		StructName: *typ,
+		Fields:     fields,
 	}
 
-	var star string
-	if se.RepetitionType != nil && *se.RepetitionType == sch.FieldRepetitionType_OPTIONAL {
-		star = "*"
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, n)
+	if err != nil {
+		log.Fatal(err)
 	}
-	return fmt.Sprintf("%s%s", star, out)
+
+	gocode, err := format.Source(buf.Bytes())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f, err := os.Create(*structOutPth)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = f.Write(gocode)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f.Close()
+	fromStruct(*structOutPth)
 }
 
-var parquetTypes = map[string]string{
-	"BOOLEAN":    "bool",
-	"INT32":      "int32",
-	"INT64":      "int64",
-	"FLOAT":      "float32",
-	"DOUBLE":     "float64",
-	"BYTE_ARRAY": "string",
-}
-
-func fromStruct() {
-	result, err := parse.Fields(*typ, *pth)
+func fromStruct(pth string) {
+	result, err := parse.Fields(*typ, pth)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -192,6 +208,27 @@ func fromStruct() {
 	}
 
 	f.Close()
+}
+
+func getFieldType(se *sch.SchemaElement) string {
+	if se.Type == nil {
+		log.Fatal("nil parquet schema type")
+	}
+	s := se.Type.String()
+	out, ok := parquetTypes[s]
+	if !ok {
+		log.Fatal("unsupported parquet schema type: %s", s)
+	}
+
+	if se.RepetitionType != nil && *se.RepetitionType == sch.FieldRepetitionType_REPEATED {
+		log.Fatalf("field %s is FieldRepetitionType_REPEATED, which is currently not supported", se.Name)
+	}
+
+	var star string
+	if se.RepetitionType != nil && *se.RepetitionType == sch.FieldRepetitionType_OPTIONAL {
+		star = "*"
+	}
+	return fmt.Sprintf("%s%s", star, out)
 }
 
 func getImport(i string) string {
---
+++ examples/via_parquet/README.md
@@ -0,0 +1,24 @@
+# Parquetgen example (by reading a parquet file)
+
+This example reads a parquet file and generates a struct,
+a ParquetReader, and a ParquetWriter.
+
+To generate the code needed to run this example:
+
+    cd cmd/parquetgen
+    go get ./...
+    go install
+    cd ../../examples/via_parquet
+    go generate
+
+Go generate calls (see the top of main.go for the go:generate command):
+
+    //parquetgen --parquet ./people.parquet --type Person --package main
+
+Which produces two files: parquet.go and generated_struct.go.  Now run:
+
+    go run .
+
+To read the parquet file.  The generated ParquetWriter is not used in
+this example (but could be used).  To see ParquetWriter in action see
+the people example.
