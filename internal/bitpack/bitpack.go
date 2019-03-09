0c95d0460e24a6c395434f25a3b8103529bac489
Craig Swank <craigswank@gmail.com>
2019-03-09 16:34:46 -0700

    added bitback.Unpack

--- internal/bitpack/bitpack.go
+++ internal/bitpack/bitpack.go
@@ -1,17 +1,75 @@
 package bitpack
 
-func Pack(vals []int64) []byte {
-	/*
-		for vals = []int64{0,1,2,3,4,5,6,7}
+func Pack(width int, vals []int64) []byte {
+	switch width {
+	case 1:
+		return packOne(vals)
+	case 2:
+		return packTwo(vals)
+	case 3:
+		return packThree(vals)
+	default:
+		return []byte{}
+	}
+}
 
-		dec value: 0   1   2   3   4   5   6   7
-		bit value: 000 001 010 011 100 101 110 111
-		bit label: ABC DEF GHI JKL MNO PQR STU VWX
+func packOne(vals []int64) []byte {
+	return []byte{
+		(byte(vals[0]) |
+			byte(vals[1]<<1) |
+			byte(vals[2]<<2) |
+			byte(vals[3]<<3) |
+			byte(vals[4]<<4) |
+			byte(vals[5]<<5) |
+			byte(vals[6]<<6) |
+			byte(vals[7]<<7)),
+	}
+}
 
-		output:
-		bit value: 10001000 11000110 11111010
-		bit label: HIDEFABC RMNOJKLG VWXSTUPQ
-	*/
+/*
+	for vals = []int64{0,1,2,3,0,1,2,3}
+
+	dec value: 0  1  2  3  0  1  2  3
+	bit value: 00 01 10 00 00 01 10 11
+	bit label: AB CD EF GH IJ KL MN OP
+
+	output:
+	bit value:
+	bit label: GHEFCDAB OPMNKLIJ
+*/
+func packTwo(vals []int64) []byte {
+	return []byte{
+		(byte(vals[0]&1) | //b
+			byte((vals[0] & 2)) | //a
+			byte((vals[1]&1)<<2) | //d
+			byte((vals[1]&2)<<2) | //c
+			byte((vals[2]&1)<<4) | //f
+			byte((vals[2]&2)<<4) | //e
+			byte((vals[3]&1)<<6) | //h
+			byte((vals[3]&2)<<6)), //g
+		(byte(vals[4]&1) | //j
+			byte(vals[4]&2) | //i
+			byte((vals[5]&1)<<2) | //l
+			byte((vals[5]&2)<<2) | //k
+			byte((vals[6]&1)<<4) | //n
+			byte((vals[6]&2)<<4) | //m
+			byte((vals[7]&1)<<6) | //p
+			byte((vals[7]&2)<<6)), //o
+	}
+}
+
+/*
+	for vals = []int64{0,1,2,3,4,5,6,7}
+
+	dec value: 0   1   2   3   4   5   6   7
+	bit value: 000 001 010 011 100 101 110 111
+	bit label: ABC DEF GHI JKL MNO PQR STU VWX
+
+	output:
+	bit value: 10001000 11000110 11111010
+	bit label: HIDEFABC RMNOJKLG VWXSTUPQ
+*/
+func packThree(vals []int64) []byte {
 	return []byte{
 		(byte(vals[0]&1) | //c
 			byte((vals[0] & 2)) | //b
@@ -37,5 +95,57 @@ func Pack(vals []int64) []byte {
 			byte((vals[7]&1)<<5) | //x
 			byte((vals[7]&2)<<5) | //w
 			byte((vals[7]&4)<<5)), //v
+	}
+}
+
+func Unpack(width int, vals []byte) []int64 {
+	switch width {
+	case 1:
+		return unpackOne(vals)
+	case 2:
+		return unpackTwo(vals)
+	case 3:
+		return unpackThree(vals)
+	default:
+		return []int64{}
+	}
+}
+
+func unpackOne(vals []byte) []int64 {
+	return []int64{
+		int64(vals[0] & 1),
+		int64((vals[0] & 2) >> 1),
+		int64((vals[0] & 4) >> 2),
+		int64((vals[0] & 8) >> 3),
+		int64((vals[0] & 16) >> 4),
+		int64((vals[0] & 32) >> 5),
+		int64((vals[0] & 64) >> 6),
+		int64((vals[0] & 128) >> 7),
+	}
+}
+
+func unpackTwo(vals []byte) []int64 {
+	return []int64{
+		int64(vals[0] & 3),
+		int64((vals[0] & (4 + 8) >> 2)),
+		int64((vals[0] & (16 + 32) >> 4)),
+		int64((vals[0] & (64 + 128)) >> 6),
+		int64(vals[1] & 3),
+		int64((vals[1] & (4 + 8) >> 2)),
+		int64((vals[1] & (16 + 32) >> 4)),
+		int64((vals[1] & (64 + 128)) >> 6),
+	}
+}
+
+func unpackThree(vals []byte) []int64 {
+	return []int64{
+		int64(vals[0] & 7),
+		int64((vals[0] & (8 + 16 + 32)) >> 3),
+		int64((vals[0] & (64 + 128) >> 6) | (vals[1]&1)<<2),
+		int64((vals[1] & (2 + 4 + 8)) >> 1),
+		int64((vals[1] & (16 + 32 + 64)) >> 4),
+		int64(((vals[1] & (128)) >> 7) | (vals[2]&3)<<1),
+		int64((vals[2] & (4 + 8 + 16)) >> 2),
+		int64((vals[2] & (32 + 64 + 128)) >> 5),
 	}
 }
--- internal/bitpack/bitpack_test.go
+++ internal/bitpack/bitpack_test.go
@@ -10,24 +10,40 @@ import (
 )
 
 type testCase struct {
-	name     string
-	input    []int64
-	expected []byte
+	name  string
+	width int
+	ints  []int64
+	bytes []byte
 }
 
-func TestBitpack(t *testing.T) {
+func TestPackAndUnpack(t *testing.T) {
 	testCases := []testCase{
 		{
-			name:     "from documentation",
-			input:    []int64{0, 1, 2, 3, 4, 5, 6, 7},
-			expected: getBytes("10001000", "11000110", "11111010"),
+			name:  "width 1",
+			width: 1,
+			ints:  []int64{0, 1, 1, 0, 0, 1, 1, 1},
+			bytes: getBytes("11100110"),
+		},
+		{
+			name:  "width 2",
+			width: 2,
+			ints:  []int64{0, 1, 2, 0, 0, 1, 2, 2},
+			bytes: getBytes("00100100", "10100100"),
+		},
+		{
+			name:  "width 3 from apache documentation",
+			width: 3,
+			ints:  []int64{0, 1, 2, 3, 4, 5, 6, 7},
+			bytes: getBytes("10001000", "11000110", "11111010"),
 		},
 	}
 
 	for i, tc := range testCases {
-		t.Run(fmt.Sprintf("%d-%s", i, tc.name), func(t *testing.T) {
-			b := bitpack.Pack(tc.input)
-			assert.Equal(t, tc.expected, b)
+		t.Run(fmt.Sprintf("%d %s", i, tc.name), func(t *testing.T) {
+			b := bitpack.Pack(tc.width, tc.ints)
+			assert.Equal(t, tc.bytes, b)
+			n := bitpack.Unpack(tc.width, b)
+			assert.Equal(t, tc.ints, n)
 		})
 	}
 }
